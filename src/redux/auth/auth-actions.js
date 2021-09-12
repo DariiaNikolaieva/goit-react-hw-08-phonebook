a540b688930c8115c4f88fa775b5621dc8f2963a
DariiaNikolaieva <nikolaieva.dariya@gmail.com>
2021-09-12 08:39:49 +0300

    feat: add operations for auth

--- src/redux/auth/auth-actions.js
+++ src/redux/auth/auth-actions.js
@@ -1,17 +1,33 @@
 import { createAction } from "@reduxjs/toolkit";
 
-export const registrationRequest = createAction("auth/registrationRequest");
-export const registrationSuccess = createAction("auth/registrationSuccess");
-export const registrationError = createAction("auth/registrationError");
+const registrationRequest = createAction("auth/registrationRequest");
+const registrationSuccess = createAction("auth/registrationSuccess");
+const registrationError = createAction("auth/registrationError");
 
-export const loginRequest = createAction("auth/loginRequest");
-export const loginSuccess = createAction("auth/loginSuccess");
-export const loginError = createAction("auth/loginError");
+const loginRequest = createAction("auth/loginRequest");
+const loginSuccess = createAction("auth/loginSuccess");
+const loginError = createAction("auth/loginError");
 
-export const logoutRequest = createAction("auth/logoutRequest");
-export const logoutSuccess = createAction("auth/logoutSuccess");
-export const logoutError = createAction("auth/logoutError");
+const logoutRequest = createAction("auth/logoutRequest");
+const logoutSuccess = createAction("auth/logoutSuccess");
+const logoutError = createAction("auth/logoutError");
 
-export const getCurrentUserRequest = createAction("auth/getCurrentUserRequest");
-export const getCurrentUserSuccess = createAction("auth/getCurrentUserSuccess");
-export const getCurrentUserError = createAction("auth/getCurrentUserError");
+const getCurrentUserRequest = createAction("auth/getCurrentUserRequest");
+const getCurrentUserSuccess = createAction("auth/getCurrentUserSuccess");
+const getCurrentUserError = createAction("auth/getCurrentUserError");
+
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default {
+  registrationRequest,
+  registrationSuccess,
+  registrationError,
+  loginRequest,
+  loginSuccess,
+  loginError,
+  logoutRequest,
+  logoutSuccess,
+  logoutError,
+  getCurrentUserRequest,
+  getCurrentUserSuccess,
+  getCurrentUserError,
+};
---
+++ src/redux/auth/auth-operations.js
@@ -0,0 +1,76 @@
+import {
+  registrationRequest,
+  registrationSuccess,
+  registrationError,
+  loginRequest,
+  loginSuccess,
+  loginError,
+  logoutRequest,
+  logoutSuccess,
+  logoutError,
+  getCurrentUserRequest,
+  getCurrentUserSuccess,
+  getCurrentUserError,
+} from "./auth-actions";
+
+import backendApi from "../../services/backend-api";
+
+const register = (credentials) => async (dispatch) => {
+  dispatch(registrationRequest());
+
+  try {
+    const { data } = await backendApi.signUp(credentials);
+
+    backendApi.token.set(data.token);
+    dispatch(registrationSuccess(data));
+  } catch (error) {
+    dispatch(registrationError(error));
+  }
+};
+
+const login = (credentials) => async (dispatch) => {
+  dispatch(loginRequest());
+
+  try {
+    const { data } = await backendApi.logIn(credentials);
+
+    backendApi.token.set(data.token);
+    dispatch(loginSuccess(data));
+  } catch (error) {
+    dispatch(loginError(error));
+  }
+};
+
+const logOut = () => async (dispatch) => {
+  dispatch(logoutRequest());
+
+  try {
+    await backendApi.logOut();
+
+    backendApi.token.unset();
+    dispatch(logoutSuccess());
+  } catch (error) {
+    dispatch(logoutError(error));
+  }
+};
+
+export const getCurrentUser = () => async (dispatch, getState) => {
+  const {
+    auth: { token: persistedToken },
+  } = getState();
+
+  if (!persistedToken) {
+    return;
+  }
+
+  backendApi.token.set(persistedToken);
+  dispatch(getCurrentUserRequest());
+
+  try {
+    const { data } = await backendApi.getCurrentUser();
+
+    dispatch(getCurrentUserSuccess(data));
+  } catch (error) {
+    dispatch(getCurrentUserError(error));
+  }
+};
